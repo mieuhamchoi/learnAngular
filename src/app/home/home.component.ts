28994a44e8d32548e42e3ed2068fba578e88fa5f
Mieu <mieuhamchoi@gmail.com>
2023-02-02 16:50:50 +0700

    use_interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-
+import { Test } from './testInterface'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
-  public name:string = 'Phuoc'
-  public age:number = 18
-  public fruitList: string[] = ['xoài', 'cam', 'bưởi']
-  public fruitListDetail = [{name: 'Xoài', price: 50368423}, {name: 'Cam', price: 20000}, {name: 'Ổi', price: 5670}] 
+
+  public vietnamProvinceList = [{name: "Chọn tỉnh thành", district: []},{name: "HCM", district: ['Q8', 'Q7', 'Q6']}, {name: "Tien Giang", district: ['My Tho', 'Trung An', 'Ha Noi']}]
+  public selectedObj:Test = {name: '', district: []}
   constructor() {}
-  public resetname = ():void => {
-    this.name = ''
-  }
+
   public ngOnInit(): void {
-    // console.log('List fruit list: ', this.fruitList)
-    // console.log('List fruit list detail: ', this.fruitListDetail)
+    //console.log("list province in VietNam", this.vietnamProvinceList)
+  }
+  public changeProvince = (event: any) => {
+    //console.log("test", this.selectedObj) // printf test {"name":"HCM","district":["Q8","Q7","Q6"]}
+    this.selectedObj = event.target.value;
+    console.log(this.selectedObj) // printf [object object]
+  }
+
+  public objToJson = (obj: object):string => {
+    return JSON.stringify(obj)
   }
 }
---
+++ src/app/home/testInterface.ts
@@ -0,0 +1,6 @@
+
+export interface Test {
+    name: string;
+    district: object[];
+}
+  
