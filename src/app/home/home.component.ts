2e9fd7fe28f739505b6ae7706e4d0a12e94124d5
Mieu <mieuhamchoi@gmail.com>
2023-02-02 17:22:39 +0700

    cascading combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,18 +10,20 @@ export class HomeComponent {
 
   public vietnamProvinceList = [{name: "Chọn tỉnh thành", district: []},{name: "HCM", district: ['Q8', 'Q7', 'Q6']}, {name: "Tien Giang", district: ['My Tho', 'Trung An', 'Ha Noi']}]
   public selectedObj:Test = {name: '', district: []}
+  public districtList:string[] = []
   constructor() {}
 
   public ngOnInit(): void {
     //console.log("list province in VietNam", this.vietnamProvinceList)
   }
   public changeProvince = (event: any) => {
-    //console.log("test", this.selectedObj) // printf test {"name":"HCM","district":["Q8","Q7","Q6"]}
-    this.selectedObj = event.target.value;
-    console.log(this.selectedObj) // printf [object object]
+    let search = event.target.value;
+    let kq = this.vietnamProvinceList.filter(province => province.name === search)
+    if(kq && kq.length > 0) {
+      this.districtList = kq[0].district
+    }else {
+      this.districtList = []
+    }
   }
 
-  public objToJson = (obj: object):string => {
-    return JSON.stringify(obj)
-  }
 }
