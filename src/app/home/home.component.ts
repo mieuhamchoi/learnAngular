d1f4a5a02fcd62520382f60dcc6849c7bfc1f860
Mieu <mieuhamchoi@gmail.com>
2023-02-02 12:29:58 +0700

    ngFor with object

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,13 @@ export class HomeComponent {
   public name:string = 'Phuoc'
   public age:number = 18
   public fruitList: string[] = ['xoài', 'cam', 'bưởi']
+  public fruitListDetail = [{name: 'Xoài', price: 50000}, {name: 'Cam', price: 20000}, {name: 'Ổi', price: 5000}] 
   constructor() {}
   public resetname = ():void => {
     this.name = ''
   }
   public ngOnInit(): void {
     console.log('List fruit list: ', this.fruitList)
+    console.log('List fruit list detail: ', this.fruitListDetail)
   }
 }
